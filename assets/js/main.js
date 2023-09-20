f3371f13d7891956e6d046eebf78484bfda10d33
Benjamin Kott <benjamin.kott@outlook.com>
2023-09-20 12:51:30 +0200

    [TASK] Enable darkmode on docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,37 @@
 // Inplace Search
-let search = document.getElementById('search');
-if (search) {
-    search.addEventListener('keyup', filterIcons);
-    function filterIcons() {
-        let searchWord = this.value.toLowerCase();
-        let iconList = document.getElementById('iconlist');
-        let icons = iconList.querySelectorAll('[data-type="icon"]');
-        for (let i = 0; i < icons.length; i++) {
-            let icon = icons[i];
-            let elementSearch = icon.dataset.search;
-            if (elementSearch.toLowerCase().indexOf(searchWord) > -1) {
-                icon.style.display = 'flex';
-            } else {
-                icon.style.display = 'none';
+document.addEventListener("DOMContentLoaded", () => {
+    let search = document.getElementById('search');
+    if (search) {
+        search.addEventListener('keyup', filterIcons);
+        function filterIcons() {
+            let searchWord = this.value.toLowerCase();
+            let iconList = document.getElementById('iconlist');
+            let icons = iconList.querySelectorAll('[data-type="icon"]');
+            for (let i = 0; i < icons.length; i++) {
+                let icon = icons[i];
+                let elementSearch = icon.dataset.search;
+                if (elementSearch.toLowerCase().indexOf(searchWord) > -1) {
+                    icon.style.display = 'flex';
+                } else {
+                    icon.style.display = 'none';
+                }
             }
         }
     }
+});
+
+// Theme
+const getPreferredTheme = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 }
+const setTheme = (theme) => {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    }
+}
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    setTheme(getPreferredTheme())
+});
+setTheme(getPreferredTheme());
